37e88c06bea066475cf467ead6313bb4c61df58d
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-03-30 22:50:48 -0700

    I change js files

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ if(close){
         nav.classList.remove('active')
     })
 }
+
+// Single product sproduct.html js code
+var MainImg=document.getElementById('MainImg');
+var smallImg=document.getElementsByClassName('small-img');
+smallImg[0].onclick=function(){
+    MainImg.src=smallImg[0].src;
+}
+smallImg[1].onclick=function(){
+    MainImg.src=smallImg[1].src;
+}
+smallImg[2].onclick=function(){
+    MainImg.src=smallImg[2].src;
+}
+smallImg[3].onclick=function(){
+    MainImg.src=smallImg[3].src;
+}  
